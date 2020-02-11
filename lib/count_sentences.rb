654f5ed98281d605bb71901eb1fbe263071f8166
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-02-11 18:58:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,4 +17,8 @@ class String
   def count_sentences
     self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
+  def count_sentences_words
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+  end
+
 end
